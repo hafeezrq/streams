db6a88f5ede92d4442d44f073ecaee7bc5135a30
hafeezrq <hafeezrq@yahoo.com>
2022-10-07 18:42:14 +1100

    Create five components and display whith BrowderRouter and Route from react-router-dom pakage.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 function App() {
-  return <div className='App'>APP</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <div>
+          <Route path='/' exact component={StreamList} />
+          <Route path='/streams/new' component={StreamCreate} />
+          <Route path='/streams/edit' component={StreamEdit} />
+          <Route path='/streams/delete' component={StreamDelete} />
+          <Route path='/streams/show' component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
