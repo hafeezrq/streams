f9c8956659dd8b06f5a277fca3e71116393c7176
hafeezrq <hafeezrq@yahoo.com>
2022-10-10 18:40:35 +1100

    Demonstrate basics of google auth2.

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 
 export class GoogleAuth extends Component {
+  state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
-      window.gapi.client.init({
-        clientId:
-          '1007311849508-24bpm5ke7f3iramnkiqtt4192jka44kv.apps.googleusercontent.com',
-        scope: 'email',
-        plugin_name: 'streamy',
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            '1007311849508-24bpm5ke7f3iramnkiqtt4192jka44kv.apps.googleusercontent.com',
+          scope: 'email',
+          plugin_name: 'streamy',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know.</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in.</div>;
+    } else {
+      return <div>I am not signed in.</div>;
+    }
+  }
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
